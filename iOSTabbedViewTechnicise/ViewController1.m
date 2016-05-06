c483f6b57e1b05b635787ec7c3910065e423858f
rita-cribello <rita.roy@technicise.com>
2016-05-06 10:37:41 +0530

    Completed sample app of tab hiding https://github.com/cribello/iosapp/issues/1031

--- iOSTabbedViewTechnicise/ViewController1.m
+++ iOSTabbedViewTechnicise/ViewController1.m
@@ -116,15 +116,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 
 ////to swipe left
@@ -152,13 +143,6 @@
         else if(ViewX==-85.000000)//-85.000000
         {
         }
-//        else
-//        {
-//            self.TabButtonsView.frame = CGRectOffset(self.TabButtonsView.frame, -90.0, 0.0);
-//            
-//        }
-        
-        //  member=@"removed";
         
     }];
     
@@ -173,7 +157,6 @@
         if(ViewX==-175.000000)//-85.000000
         {
             self.TabButtonsView.frame = CGRectOffset(self.TabButtonsView.frame, 175.0, 0.0);//85.0
-           // swipe=@"left";
         }
         
         else if(ViewX==-85.000000)
